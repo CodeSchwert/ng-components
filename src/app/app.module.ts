62bda4fcb3c94b4c9fbcf9b6d9be64fea784ad92
Nik <nik@parasol.ai>
2021-08-18 18:20:47 +1200

    Home and Not found pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,23 @@ import { CollectionsModule } from './collections/collections.module';
 import { ElementsModule } from './elements/elements.module';
 import { ModsModule } from './mods/mods.module';
 import { ViewsModule } from './views/views.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     CollectionsModule,
     ElementsModule,
     ModsModule,
-    ViewsModule
+    ViewsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
